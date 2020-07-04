034af2712a0b72319d6f04a6b6f7ecf1ca257876
William Santiago Alzate <santiago_alza@hotmail.com>
2020-07-04 18:04:21 -0500

    Creacion del proyecto de votes-app con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Link } from './link/link.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-links-votes-app';
+  links: Link[];
+
+  constructor() {
+    this.links = [
+      new Link('Angular', 'http://angular.io', 100),
+      new Link('Google', 'http://google.com', 30),
+      new Link('Faztweb', 'http://faztweb.com', 200),
+    ];
+  }
+
+  addLink(title: HTMLInputElement, link: HTMLInputElement) {
+    this.links.push(new Link(title.value, link.value))
+    title.value = '';
+    link.value = '';
+    return false
+  }
 }
